330d262b1c582b111b2987f40d86de51b1a6358b
Rodomers <g.rodomers@gmail.com>
2025-09-25 21:41:54 +0300

    Живое

---
+++ src/Classes.kt
@@ -0,0 +1,96 @@
+enum class WorkFee(val rate: Double) {
+    HIGH(50.0),
+    MIDDLE(40.0),
+    LOW(35.0)
+}
+
+data class Work(val name: String, val fee: WorkFee)
+
+class Employee(val surname: String) {
+    val works = mutableListOf<Work>()
+
+    fun addWork(work: Work) {
+        works.add(work)
+    }
+
+    fun countFee(): Double {
+        return works.sumOf { it.fee.rate }
+    }
+}
+
+object PayrollDepartment {
+    private val employees = mutableListOf<Employee>()
+    private val workTypes = mutableListOf<Work>()
+
+    private fun getEmployeeId(surname: String): Int {
+        return employees.indexOfFirst { it.surname == surname }
+    }
+
+    private fun getWorkId(workName: String): Int {
+        return workTypes.indexOfFirst { it.name == workName }
+    }
+
+    fun addEmployee(employee: Employee): Boolean {
+        if (getEmployeeId(employee.surname) == -1) {
+            employees.add(employee)
+            return true
+        }
+        return false
+    }
+
+    fun deleteEmployee(surname: String): Boolean {
+        val employeeId = getEmployeeId(surname)
+        if (employeeId != -1) {
+            employees.removeIf { it.surname == surname }
+            return true
+        }
+        return false
+    }
+
+    fun addWorkType(work: Work): Boolean {
+        if (getWorkId(work.name) == -1) {
+            workTypes.add(work)
+            return true
+        }
+        return false
+    }
+
+    fun addEmployeeDoneWork(surname: String, workName: String): Boolean {
+        val employeeId = getEmployeeId(surname)
+        val workId = getWorkId(workName)
+        if (employeeId != -1 && workId != -1) {
+            employees[employeeId].addWork(workTypes[workId])
+            return true
+        }
+        return false
+    }
+
+    fun countEmployeeFee(surname: String): Double? {
+        val employeeId = getEmployeeId(surname)
+
+        if (employeeId != -1) {
+            return employees[employeeId].countFee()
+        }
+
+        return null
+    }
+
+    fun countFeesSum(): Double {
+        return employees.sumOf { it.countFee() }
+    }
+
+    fun printWorkTypes(full: Boolean = false): String {
+        return when (full) {
+            true -> workTypes.joinToString("\n") { it -> "${it.name}: ${it.fee} (${it.fee.rate})" }
+            else -> workTypes.joinToString(", ") { it -> it.name }
+        }
+    }
+
+    fun printEmployees(): String {
+        return employees.joinToString(", ") { it -> it.surname }
+    }
+
+    fun checkEmployee(surname: String): Boolean {
+        return PayrollDepartment.getEmployeeId(surname) != -1
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,157 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.util.Scanner
+import kotlin.system.exitProcess
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+val scan = Scanner(System.`in`)
+
+fun clearConsole(fake: Boolean = true) {
+    if (!fake) {
+        print("\u001b[H\u001b[2J")
+        System.out.flush()
     }
+    else {
+        for (i in 0..50) {
+            println()
+        }
+    }
+}
+
+fun addWorkMenu() {
+    clearConsole()
+    while (true) {
+        println("Введите название работы или 0 для выхода")
+
+        val line = scan.nextLine()
+        if (line != "0") {
+            val name = line.trim()
+            var feeLevel: String
+            do {
+                println("Выберите уровень оплаты\n1. Низкий\n2. Средний\n3. Высокий")
+                feeLevel = scan.nextLine()
+            } while (feeLevel.trim() !in listOf("1", "2", "3"))
+
+            when (feeLevel.toInt()) {
+                3 -> PayrollDepartment.addWorkType(
+                    Work(name, WorkFee.HIGH)
+                )
+                2 -> PayrollDepartment.addWorkType(
+                    Work(name, WorkFee.MIDDLE)
+                )
+                else -> PayrollDepartment.addWorkType(
+                    Work(name, WorkFee.LOW)
+                )
+            }
+        }
+
+//        println(PayrollDepartment.printWorkTypes())
+
+        mainMenu()
+        break
+    }
+}
+
+fun employeeMenu(
+    del: Boolean = false,
+    addWork: Boolean = false,
+    countFee: Boolean = false
+    ) {
+    val additionalStr = when {
+        del -> "Вы выбрали удаление работника.\nСписок работников предприятия:\n${PayrollDepartment.printEmployees()}."
+        addWork -> "Список работников предприятия:\n${PayrollDepartment.printEmployees()}.\nКем была выполнена работа?"
+        countFee -> "Вы выбрали подсчёт выплаты работнику.\nСписок работников предприятия:\n${PayrollDepartment.printEmployees()}."
+        else -> "Вы выбрали добавление нового работника."
+    }
+
+    clearConsole()
+    while (true) {
+        println("$additionalStr\nВведите фамилию работника или 0 для выхода")
+
+        val line = scan.nextLine()
+        var res: String? = null
+        if (line != "0") {
+            val name = line.trim()
+            when {
+                del -> {
+                    res = if (!PayrollDepartment.deleteEmployee(name)) "Работник с такой фамилией не найден."
+                    else "$name уволен(а)."
+                }
+                addWork -> {
+                    if (!PayrollDepartment.checkEmployee(name)) res = "Такого работника не существует"
+                    else {
+                        println("Существующие в компании виды работ: ${PayrollDepartment.printWorkTypes()}\n" +
+                                "Введите вид работы, который выполнил(а) $name.")
+                        val type = scan.nextLine().trim()
+                        if (!PayrollDepartment.addEmployeeDoneWork(name, type)) res = "Такого вида работ не существует."
+                    }
+                }
+                countFee -> {
+                    val fee = PayrollDepartment.countEmployeeFee(name)
+                    res = when (fee) {
+                        null -> "Такого работника не существует."
+                        else -> "Сумма выплат для работника $name: $fee"
+                    }
+                }
+                else -> {
+                    if (!PayrollDepartment.addEmployee(Employee(name))) res = "Работник с такой фамилией уже существует."
+                }
+            }
+
+        }
+
+        mainMenu(text = res ?: "")
+        break
+    }
+}
+
+fun workTypesMenu() {
+    clearConsole()
+    println("Список видов работ, доступных на предприятии:\n${PayrollDepartment.printWorkTypes(full = true)}\n\n" +
+            "Нажмите любую клавишу для возврата в главное меню")
+    scan.nextLine()
+    mainMenu()
+}
+
+fun mainMenu(text: String = "") {
+    clearConsole()
+    if (text != "") println(text)
+    while (true) {
+        println("Меню:")
+        println("1. Добавить вид работы")
+        println("2. Добавить работника")
+        println("3. Удалить работника")
+        println("4. Добавить выполненную работу")
+        println("5. Посчитать зарплату работника")
+        println("6. Посчитать зарплату всех работников")
+        println("7. Получить список работников предприятия")
+        println("8. Получить список видов работ")
+        println("0. Выход из программы")
+
+        val line = scan.nextLine()
+        val intLine = try {
+            line.toInt()
+        } catch (e: Exception) {
+            mainMenu()
+        }
+
+        when (intLine) {
+            1 -> addWorkMenu()
+            2 -> employeeMenu()
+            3 -> employeeMenu(del = true)
+            4 -> employeeMenu(addWork = true)
+            5 -> employeeMenu(countFee = true)
+            6 -> {
+                mainMenu("Общая сумма выплат работников: ${PayrollDepartment.countFeesSum()}.")
+            }
+            7 -> mainMenu("Список работников предприятия: ${PayrollDepartment.printEmployees()}")
+            8 -> workTypesMenu()
+            else -> {
+                clearConsole()
+                println("Выход...")
+                exitProcess(1)
+            }
+        }
+    }
+}
+
+fun main() {
+    mainMenu()
 }
